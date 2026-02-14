6109d797c78571780df6737de1cdca978a0b620d
Benjamin <gabonnie1234@gmail.com>
2026-02-14 06:14:26 -0500

    feat: deno-run with arguments

--- src/deno-run.cbl
+++ src/deno-run.cbl
@@ -3,21 +3,23 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  CMD-BUFFER      PIC X(300).
+       01  CMD-BUFFER      PIC X(500).
        01  RET-VAL         PIC S9(4) COMP.
 
        LINKAGE SECTION.
        01  FILE-PATH       PIC X(100).
        01  FLAGS           PIC X(50).
+       01  ARGUMENTS       PIC X(200).
        01  STATUS-CODE     PIC S9(4) COMP.
 
-       PROCEDURE DIVISION USING FILE-PATH FLAGS STATUS-CODE.
+       PROCEDURE DIVISION USING FILE-PATH FLAGS ARGUMENTS STATUS-CODE.
            MOVE SPACES TO CMD-BUFFER
            STRING "deno run "              DELIMITED BY SIZE
                   FUNCTION TRIM(FLAGS)     DELIMITED BY SIZE
                   " '"                     DELIMITED BY SIZE
                   FUNCTION TRIM(FILE-PATH) DELIMITED BY SIZE
-                  "'"                      DELIMITED BY SIZE
+                  "' "                     DELIMITED BY SIZE
+                  FUNCTION TRIM(ARGUMENTS) DELIMITED BY SIZE
                   INTO CMD-BUFFER
 
            CALL "SYSTEM" USING CMD-BUFFER RETURNING RET-VAL
